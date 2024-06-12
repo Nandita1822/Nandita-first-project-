0de72171da5ad2f2f228d0905f4ff52c015b1521
Nandita1822 <144609812+Nandita1822@users.noreply.github.com>
2024-06-12 18:32:16 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -5,7 +5,28 @@ const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 const multer = require('multer');
-const { storage } = require("../config/cloudconfig.js");
+const cloudinary =require('cloudinary');
+const{CloudinaryStorage}= require('multer-storage-cloudinary');
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: 'wanderlust_DEV',
+      allowedFormat:["png" , "jpg", "jpeg"],
+
+    },
+  });
+
+module.exports={
+    cloudinary,
+    storage,
+};
 
 const upload = multer({ storage });
 
