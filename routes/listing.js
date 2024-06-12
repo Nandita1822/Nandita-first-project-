65405515323b1320c3d8882b39d39f4deaa1c8d5
Nandita1822 <144609812+Nandita1822@users.noreply.github.com>
2024-06-12 18:33:34 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -1,55 +1,37 @@
-const express = require("express");
+const express =require("express");
 const router = express.Router();
-const wrapAsync = require("../utils/wrapAsync.js");
+const wrapAsync=require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
-const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const {isLoggedIn,isOwner,validateListing }=require("../middleware.js");
 const listingController = require("../controllers/listing.js");
-const multer = require('multer');
-const cloudinary =require('cloudinary');
-const{CloudinaryStorage}= require('multer-storage-cloudinary');
-
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-});
-
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: 'wanderlust_DEV',
-      allowedFormat:["png" , "jpg", "jpeg"],
-
-    },
-  });
-
-module.exports={
-    cloudinary,
-    storage,
-};
-
+const multer  = require('multer');
+const {storage}=require("../cloudconfig.js");
 const upload = multer({ storage });
+router
+.route("/")
+.get(wrapAsync(listingController.index))
+.post(isLoggedIn,
+   upload.single('listing[image]'),
+   validateListing,
+  wrapAsync(listingController.createListing ));
 
-router.route("/")
-  .get(wrapAsync(listingController.index))
-  .post(isLoggedIn,
-    upload.single('listing[image]'),
-    validateListing,
-    wrapAsync(listingController.createListing)
-  );
 
-// New Route
-router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-router.route("/:id")
+  //New Route
+  router.get("/new",isLoggedIn,listingController.renderNewForm);
+
+
+
+  router
+  .route("/:id")
   .get(wrapAsync(listingController.showListing))
-  .put(isLoggedIn, isOwner, upload.single('listing[image]'), validateListing,
-    wrapAsync(listingController.updateListing))
-  .delete(isLoggedIn, isOwner,
-    wrapAsync(listingController.destroyListing)
-  );
+   .put(isLoggedIn,isOwner,upload.single('listing[image]'),validateListing,
+      wrapAsync(listingController.updateListing))
+   .delete(isLoggedIn,isOwner,
+        wrapAsync(listingController.destroyListing));
 
-// Edit Route
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
+ 
+//Edit Route
+router.get("/:id/edit",isLoggedIn,isOwner, wrapAsync(listingController.renderEditForm));
 
 module.exports = router;
